fed33d9837e6677aae3619b2d07f415e6fc206af
Everaldo Júnior <everaldo.junior98@live.com>
2022-03-12 15:41:11 -0300

    First commit
    Adding initial project;
    Adding readme, license and gitignore files;

---
+++ CircuitSharp/Demo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CircuitSharp.Components;
+using CircuitSharp.Components.Base;
+using CircuitSharp.Core;
+
+namespace Demo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var sim = new Circuit();
+
+            var volt0 = sim.Create<VoltageInput>(Voltage.WaveType.Dc);
+            volt0.SetMaxVoltage(10);
+            var res0 = sim.Create<Resistor>(100);
+            var ground0 = sim.Create<Ground>();
+
+            sim.Connect(volt0.LeadPos, res0.LeadIn);
+            sim.Connect(res0.LeadOut, ground0.LeadIn);
+
+            for (var x = 1; x <= 100; x++)
+                sim.DoTick();
+
+            Console.WriteLine($"{res0.GetVoltageDelta()} {res0.GetCurrent()}");
+            Console.ReadLine();
+        }
+    }
+}
